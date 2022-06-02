7d469c7180a313fae1af0aaf73f46f8057fbb10b
Wajeah Raja <rajawajeah@gmail.com>
2022-06-02 06:21:14 -0400

    doing further work on the user queus page

--- src/components/Userqueue.js
+++ src/components/Userqueue.js
@@ -7,11 +7,10 @@ function Userqueue() {
     const location = useLocation();
     const URL = location.state.URL
     const token = location.state.token
-    const [bugIds, setBugIds] = useState({})
     const [queState, setQueState] = useState("")
     // FOR REFRENCE WHEN YOU WAKE UP YOU WERE WORKING ON THIS ROUTE IMPORT USELOCATION AND GET THE TOKEN OUT SO U CAN REMOVE THE EXTRA REQUEST
     useEffect ( ()=> {
-        const fetchBugIdFromUser = async()=> {
+        const fetchBugFromUser = async()=> {
             const res = await fetch(URL+ '/user/login', {
             method: 'get',
             headers: {
@@ -20,43 +19,41 @@ function Userqueue() {
                 }
             })
             const userData = await res.json();
-            setBugIds(userData.bugsQue)
-        }
-        const fetchBug =async()=> {
-            const res = await fetch(URL+ '/bugs', {
-                method: 'post',
-                headers: {
-                    "Content-Type": "application/json",
-                    "Authorization": `Bearer ${token.accessToken}`
-                },
-                body: JSON.stringify({
-                    ids: bugIds
+           const ids = userData.bugsQue
+            console.log(userData.bugsQue)
+                const res2 = await fetch(URL+ '/bugs', {
+                    method: 'post',
+                    headers: {
+                        "Content-Type": "application/json",
+                        "Authorization": `Bearer ${token.accessToken}`
+                    },
+                    body: JSON.stringify({
+                        id: ids
+                    })})
+                const bugData = await res2.json();
+                console.log(bugData)
+                const newData = await bugData.bugQue
+                const testData = await bugData.tests
+                console.log(testData)
+                console.log(newData)
+                const bugTests = testData.map((data,index)=>{
+                    return (
+                        <div key={index}>
+                            {data.body}
+                        </div>
+                    )
                 })
-            })
-            const bugData = await res.json();
-            console.log(bugData)
-            const newData = await bugData.bugQue
-            const testData = await bugData.testArr
-            console.log(testData)
-            console.log(newData)
-            const bugTests = testData.map((data,index)=>{
-                return (
-                    <div key={index}>
-                        {data.body}
-                    </div>
-                )
-            })
-            const bugQueues =newData.map((data,index)=> {
-                console.log(bugTests)
-                return (
-                        <Link key={index} state={{bug:data.codeBase,tests:testData[index]}} to={'bugview'}> Bug # {index+1}</Link>
-                )
-            })
-            setQueState(bugQueues)
-            console.log(bugQueues)
+                const bugQueues =newData.map((data,index)=> {
+                    console.log(bugTests)
+                    return (
+                            <Link key={index} state={{bug:data.codeBase,tests:testData[index]}} to={'bugview'}> Bug # {index+1}</Link>
+                    )
+                })
+                setQueState(bugQueues)
+                console.log(bugQueues)
+
         }
-        fetchBugIdFromUser()
-        fetchBug()
+        fetchBugFromUser()
         }, [])
     return (
         <div>
